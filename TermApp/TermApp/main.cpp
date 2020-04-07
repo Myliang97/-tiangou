d54d95f3c4015bba3d5f8ef2d75e5940003116b4
Myliang97 <1467972160@qq.com>
2020-04-07 16:42:34 +0800

    se

--- TermApp/TermApp/main.cpp
+++ TermApp/TermApp/main.cpp
@@ -11,7 +11,7 @@ DWORD curProcId = 0;
 
 bool InitTipWindow()
 {
-
+	return true;
 }
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
